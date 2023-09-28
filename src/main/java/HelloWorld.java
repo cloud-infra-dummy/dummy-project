9bd58df8396163e9d666c42ec6b224e688b74507
Rui Chen <rchen4@andrew.cmu.edu>
2023-09-28 00:12:37 -0700

    change file

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+	public static void test() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestTest {
+    @Test
+    public void test() {
+        HelloWorld.test();
+    }
+}
